cf72e6c1a82b4b7314fcaf45d05087b91f1c2f99
Megan Hepner <megan.hepner@gmail.com>
2020-06-22 15:50:34 -0700

    create html form and jquery form submit function
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
-// import { pingPong } from './ping-pong';
+import { } from './journal';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function() {
+  $('#journal').submit(function(event) {
+    event.preventDefault();
+    const date = $("#journal-date").val();
+    const entry = $("#journal-entry").val();
+  });
+});
